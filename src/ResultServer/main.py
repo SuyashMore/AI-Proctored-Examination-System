a8aeb9fae1cbe754351e30498c6b3d5487849806
hrimG <hrimgandhi@gmail.com>
2020-11-01 00:14:58 +0530

    score and store added

--- src/ResultServer/main.py
+++ src/ResultServer/main.py
@@ -98,10 +98,61 @@ class getStudentResponse(Resource):
         data = {"result": bigjson}
         return jsonify(data)
 
+class scoreAndStore(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('response', type=dict, required=True, help="This field cannot be left blank.")
+    
+    def __init__(self):
+        db.connect()
+    
+    def post(self):
+        print("score and store was called")
+        headers: { "Accept": "application/json", "Content-type": "application/json",}
+        response = scoreAndStore.parser.parse_args()["response"]
+        print(type(response))
+        t_id = str(response["test_id"])
+        s_id = str(response["student_id"])
+        mcq_score =0
+        final_score=0
+        generated=False
+        answers=[]
+        integrity=0
+        subjective_score=0
+        qna = response["qna"]
+        for q in qna:
+            if(q["actualAnswer"]!="This is a sbjective question"):
+                if(q["actualAnswer"]==q["studentAnswer"]):
+                    mcq_score = mcq_score +1
+            answers.append(q["studentAnswer"])
+        print(qna)
+        print(type(qna))
+        print(mcq_score)
+        print(answers)
+        
+        jsondata = {
+                'student_id': s_id, 
+                'test_id': t_id,
+                'mcq_score':mcq_score,
+                'final_score':final_score,
+                'generated':generated,
+                'answers':answers,
+                'integrity':integrity,
+                'subjective_score':subjective_score
+            }
+
+        
+
+        result = db.insertEntry("result",jsondata)
+        print(result)
+        
+        data = {"result": "success"}
+        return jsonify(data)
+
 
 api.add_resource(getStudentResult, '/getStudentResult')
 api.add_resource(getTestResult, '/getTestResult')
 api.add_resource(getStudentResponse, '/getStudentResponse')
+api.add_resource(scoreAndStore, '/scoreAndStore')
 
 if __name__ == "__main__":
     db = dbManager()
